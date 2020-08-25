63e412b08a30516fed391ac4684bada5273ab842
Rahul Raj Tripathi <tripathirahul728@gmail.com>
2020-08-25 14:08:55 +0530

    Home-Login
    25th August 2020

---
+++ op.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("reset").reset();
+    document.getElementById("id01").style.display='none';
+}
+function on() {
+  document.getElementById("id01").style.display = "block";
+}
+
+function off() {
+  document.getElementByClass("overlay").style.display = "none";
+}
